7e23e6876d52cb25e54296bee205c7bd9b377e3e
juanCruzSalina <salinajuancruz@gmail.com>
2023-03-26 05:34:55 -0300

    db installed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,16 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { UserModule } from './user/user.module';
-import { DataModule } from './data/data.module';
-import { GuildModule } from './guild/guild.module';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [UserModule, DataModule, GuildModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true
+    }),
+    MongooseModule.forRoot(process.env.MONGO_URL)
+  ],
   controllers: [AppController],
   providers: [AppService]
 })
